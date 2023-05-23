3e03c8be997edd89e492924523e4f239e4304909
davidcoderistov <davidcoderistov@gmail.com>
2023-05-23 17:45:48 +0200

    Create Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import LoadingButton from '@mui/lab/LoadingButton'
+
+
+interface ButtonProps {
+    variant: 'primary' | 'secondary'
+    text: string
+    minWidth?: number
+    contained: boolean
+    loading: boolean
+    onClick: (event: React.MouseEvent) => void
+}
+
+export default function Button(props: ButtonProps) {
+
+    const primary = props.variant === 'primary'
+
+    return (
+        <LoadingButton
+            variant={props.contained ? 'contained' : 'text'}
+            sx={props.contained ? {
+                textTransform: 'none',
+                borderRadius: '10px',
+                ...!!props.minWidth && { minWidth: props.minWidth },
+                backgroundColor: primary ? '#0095F6' : '#EFEFEF',
+                ...!primary && { color: '#000000' },
+                '&:hover': {
+                    backgroundColor: primary ? '#1877F2' : '#DBDBDB',
+                },
+                '&.MuiLoadingButton-loading': {
+                    backgroundColor: primary ? '#0095F6' : '#FFFFFF',
+                },
+                '.MuiLoadingButton-loadingIndicator': {
+                    color: primary ? '#FFFFFF' : '#000000',
+                },
+                ...props.text === 'Follow' && { paddingX: '20px' },
+                ...props.text === 'Following' && { paddingX: '24px' },
+            } : {
+                textTransform: 'none',
+                color: primary ? '#0095F6' : '#DBDBDB',
+                '&:hover': {
+                    color: '#FFFFFF',
+                },
+                '.MuiLoadingButton-loadingIndicator': {
+                    color: primary ? '#0095F6' : '#DBDBDB',
+                },
+                paddingX: 0,
+                ...props.text === 'Follow' && { minWidth: '46px' },
+            }}
+            loading={props.loading}
+            onClick={props.onClick}
+        >
+            {props.text}
+        </LoadingButton>
+    )
+}
