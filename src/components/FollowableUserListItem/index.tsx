2321fb6f98ddf11dd3995ae3f49bdccaa59bcddb
davidcoderistov <davidcoderistov@gmail.com>
2023-06-05 11:26:35 +0200

    Remove unused import

--- src/components/FollowableUserListItem/index.tsx
+++ src/components/FollowableUserListItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import { useState, useCallback } from 'react'
 import Box from '@mui/material/Box'
 import Button from '../Button'
 import Skeleton from '@mui/material/Skeleton'
