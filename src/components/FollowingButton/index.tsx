64105059547a20465774279b5276ba6f4d75a7f3
davidcoderistov <davidcoderistov@gmail.com>
2023-06-06 12:42:58 +0200

    Create FollowingButton component

---
+++ src/components/FollowingButton/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useCallback } from 'react'
+import Button from '../Button'
+import UnfollowUserModal from '../UnfollowUserModal'
+
+
+interface Props {
+    contained: boolean
+    user: {
+        id: string | number
+        username: string
+        photoUrl: string
+        followingLoading: boolean
+    }
+
+    onUnfollowUser(id: string | number): void
+}
+
+export default function FollowingButton(props: Props) {
+
+    const [unfollowUserModalOpen, setUnfollowUserModalOpen] = useState(false)
+
+    const handleOpenUnfollowUserModal = () => {
+        setUnfollowUserModalOpen(true)
+    }
+
+    const handleCloseUnfollowUserModal = () => {
+        setUnfollowUserModalOpen(false)
+    }
+
+    const handleUnfollowUser = useCallback(() => {
+        setUnfollowUserModalOpen(false)
+        props.onUnfollowUser(props.user.id)
+    }, [props.onUnfollowUser])
+
+    return (
+        <>
+            <Button
+                variant='secondary'
+                text='Following'
+                contained
+                loading={props.user.followingLoading}
+                onClick={handleOpenUnfollowUserModal}
+            />
+            <UnfollowUserModal
+                open={unfollowUserModalOpen}
+                user={props.user}
+                onUnfollowUser={handleUnfollowUser}
+                onCloseModal={handleCloseUnfollowUserModal}
+            />
+        </>
+    )
+}
