3a59f0c13112b39d442de37c3783002ff41aaf9b
davidcoderistov <davidcoderistov@gmail.com>
2023-06-11 16:08:10 +0200

    Create SidebarDrawer component

---
+++ src/components/SidebarDrawer/index.tsx
@@ -0,0 +1,28 @@
+import MuiDrawer from '@mui/material/Drawer'
+import { styled } from '@mui/material/styles'
+
+
+const SidebarDrawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: 390,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
+                width: 0,
+            }),
+        },
+    }),
+)
+
+export default SidebarDrawer
