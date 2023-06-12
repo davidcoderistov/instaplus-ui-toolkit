2fc95eb04f05bbcc6e6f12228294a0aba4276751
davidcoderistov <davidcoderistov@gmail.com>
2023-06-12 15:20:53 +0200

    Introduce granularity to time elapsed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,19 +20,37 @@ export function formatNumber(number: number) {
     return number.toString()
 }
 
-export const getTimeElapsed = (timestamp: number) => {
+export const getTimeElapsed = (timestamp: number, granularity?: 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks') => {
     const now = moment()
     const ago = moment(timestamp)
 
-    if (now.diff(ago, 'seconds') < 1) {
+    let granularityOrder
+    switch (granularity) {
+        case 'minutes':
+            granularityOrder = 1
+            break
+        case 'hours':
+            granularityOrder = 2
+            break
+        case 'days':
+            granularityOrder = 3
+            break
+        case 'weeks':
+            granularityOrder = 4
+            break
+        default:
+            granularityOrder = 0
+    }
+
+    if (now.diff(ago, 'seconds') < 1 && 0 >= granularityOrder) {
         return 'Now'
-    } else if (now.diff(ago, 'minutes') < 1) {
+    } else if (now.diff(ago, 'minutes') < 1 && 0 >= granularityOrder) {
         return `${now.diff(ago, 'seconds')}s`
-    } else if (now.diff(ago, 'minutes') < 60) {
+    } else if (now.diff(ago, 'minutes') < 60 && 1 >= granularityOrder) {
         return `${now.diff(ago, 'minutes')}m`
-    } else if (now.diff(ago, 'hours') < 24) {
+    } else if (now.diff(ago, 'hours') < 24 && 2 >= granularityOrder) {
         return `${now.diff(ago, 'hours')}h`
-    } else if (now.diff(ago, 'days') < 7) {
+    } else if (now.diff(ago, 'days') < 7 && 3 >= granularityOrder) {
         return `${now.diff(ago, 'days')}d`
     } else {
         return `${now.diff(ago, 'weeks')}w`
