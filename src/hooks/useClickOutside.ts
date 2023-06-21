98c8dc1790e9dd9deab2ba4b3ffbe65d0e8d3495
davidcoderistov <davidcoderistov@gmail.com>
2023-06-21 13:45:35 +0200

    Use mouseup event for click outside hook

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -3,15 +3,15 @@ import React, { useEffect } from 'react'
 
 export function useClickOutside(ref: React.MutableRefObject<Node | null>, cb: (target: Node) => void) {
     useEffect(() => {
-        function handleClickOutside(event: DocumentEventMap['mousedown']) {
+        function handleClickOutside(event: DocumentEventMap['mouseup']) {
             if (ref.current && !ref.current?.contains(event.target as Node)) {
                 cb(event.target as Node)
             }
         }
 
-        document.addEventListener('mousedown', handleClickOutside)
+        document.addEventListener('mouseup', handleClickOutside)
         return () => {
-            document.removeEventListener('mousedown', handleClickOutside)
+            document.removeEventListener('mouseup', handleClickOutside)
         }
     }, [cb, ref])
 }
