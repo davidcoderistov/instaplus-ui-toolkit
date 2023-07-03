0992b3deac4cabba58f2b4520ce772352b78f4a7
davidcoderistov <davidcoderistov@gmail.com>
2023-07-03 14:08:14 +0200

    Add no photo url avatar in ChatAvatar component

--- src/components/ChatAvatar/index.tsx
+++ src/components/ChatAvatar/index.tsx
@@ -1,10 +1,12 @@
 import { useMemo } from 'react'
 import Box from '@mui/material/Box'
+import Avatar from '@mui/material/Avatar'
 import Skeleton from '@mui/material/Skeleton'
 
 
 interface StaticProps {
     loading?: never
+    multiple?: boolean
     photoUrls: string[]
     containerSize: number
     avatarSize: number
@@ -13,6 +15,7 @@ interface StaticProps {
 
 interface LoadingProps {
     loading: true
+    multiple?: never
     photoUrls?: never
     containerSize: number
     avatarSize?: never
@@ -43,22 +46,22 @@ export default function ChatAvatar(props: Props) {
                     sx={{ backgroundColor: '#202020' }} />
             ) : (
                 <>
-                    {props.photoUrls.length > 0 && (
-                        <Box
-                            component='span'
-                            height={avatarSizePx}
-                            width={avatarSizePx}
-                            position='relative'
-                            display='block'
-                            sx={{
-                                borderBottomLeftRadius: '50%',
-                                borderTopRightRadius: '50%',
-                                overflowX: 'hidden',
-                                borderBottomRightRadius: '50%',
-                                borderTopLeftRadius: '50%',
-                                overflowY: 'hidden',
-                            }}
-                        >
+                    <Box
+                        component='span'
+                        height={avatarSizePx}
+                        width={avatarSizePx}
+                        position='relative'
+                        display='block'
+                        sx={{
+                            borderBottomLeftRadius: '50%',
+                            borderTopRightRadius: '50%',
+                            overflowX: 'hidden',
+                            borderBottomRightRadius: '50%',
+                            borderTopLeftRadius: '50%',
+                            overflowY: 'hidden',
+                        }}
+                    >
+                        {props.photoUrls.length > 0 ? (
                             <img alt='User avatar'
                                  style={{
                                      height: '100%',
@@ -70,9 +73,11 @@ export default function ChatAvatar(props: Props) {
                                  height={`${props.avatarSize}`}
                                  src={props.photoUrls[0]}
                                  width={`${props.avatarSize}`} />
-                        </Box>
-                    )}
-                    {props.photoUrls.length > 1 && (
+                        ) : (
+                            <Avatar sx={{ height: props.avatarSize, width: props.avatarSize }} />
+                        )}
+                    </Box>
+                    {props.multiple && (
                         <Box
                             component='div'
                             bgcolor='#000000'
@@ -107,17 +112,21 @@ export default function ChatAvatar(props: Props) {
                                     overflowY: 'hidden',
                                 }}
                             >
-                                <img alt='User avatar'
-                                     style={{
-                                         height: '100%',
-                                         width: '100%',
-                                         border: '0',
-                                         overflowClipMargin: 'content-box',
-                                         overflow: 'clip',
-                                     }}
-                                     height={`${props.avatarSize}`}
-                                     src={props.photoUrls[1]}
-                                     width={`${props.avatarSize}`} />
+                                {props.photoUrls.length > 1 ? (
+                                    <img alt='User avatar'
+                                         style={{
+                                             height: '100%',
+                                             width: '100%',
+                                             border: '0',
+                                             overflowClipMargin: 'content-box',
+                                             overflow: 'clip',
+                                         }}
+                                         height={`${props.avatarSize}`}
+                                         src={props.photoUrls[1]}
+                                         width={`${props.avatarSize}`} />
+                                ) : (
+                                    <Avatar sx={{ height: props.avatarSize, width: props.avatarSize }} />
+                                )}
                             </Box>
                         </Box>
                     )}
