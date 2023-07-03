765797adf11a3538824b69aee383afe19f92b214
davidcoderistov <davidcoderistov@gmail.com>
2023-07-03 13:43:14 +0200

    Create PasswordInput component

---
+++ src/components/PasswordInput/index.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+
+
+interface Props {
+    id: string
+    label: string
+    error: boolean
+    errorMessage: string
+}
+
+const PasswordInput = React.forwardRef<HTMLInputElement, Props>(({ id, label, error, errorMessage, ...rest }, ref) => {
+
+    const [showPassword, setShowPassword] = useState(false)
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+    }
+
+    const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+    }
+
+    return (
+        <TextField
+            {...rest}
+            ref={ref}
+            error={error}
+            helperText={error && errorMessage ? errorMessage : ''}
+            required
+            fullWidth
+            id={id}
+            label={label}
+            type={showPassword ? 'text' : 'password'}
+            autoComplete={id}
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position='end'>
+                        <IconButton
+                            aria-label='toggle password visibility'
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            onMouseUp={handleMouseUpPassword}
+                            edge='end'
+                        >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }}
+        />
+    )
+})
+
+export default PasswordInput
