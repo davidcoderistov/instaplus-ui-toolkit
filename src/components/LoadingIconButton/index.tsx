c7140243adc014e68cec3302943e790b6b23fc0f
davidcoderistov <davidcoderistov@gmail.com>
2023-09-03 18:44:39 +0200

    Create LoadingIconButton component

---
+++ src/components/LoadingIconButton/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import CircularProgress from '@mui/material/CircularProgress'
+
+
+interface Props {
+    color: string
+    loading: boolean
+    iconComponent: React.ReactNode
+    onClick: () => void
+}
+
+export default function LoadingIconButton({ color, loading, iconComponent, onClick }: Props) {
+
+    const size = iconComponent.props?.sx?.fontSize || 24
+
+    return loading ? (
+        <Button
+            sx={{
+                padding: '11px',
+                margin: 0,
+                minWidth: 0,
+            }}
+            disabled={true}
+        >
+            <CircularProgress size={size - 6} sx={{ color }} />
+        </Button>
+    ) : (
+        <IconButton
+            sx={{ color }}
+            onClick={onClick}
+        >
+            {iconComponent}
+        </IconButton>
+    )
+}
+
